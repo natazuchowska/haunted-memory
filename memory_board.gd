4f5f60610d16f590ecf52b491d6dbf16257d0877
natazuchowska <98778016+natazuchowska@users.noreply.github.com>
2024-10-12 14:18:28 +0200

    moving children added

--- memory_board.gd
+++ memory_board.gd
@@ -3,6 +3,9 @@ extends Node2D
 @onready var v_box_container: VBoxContainer = $VBoxContainer
 var card_count = 1 # determine if it's the first or 2nd card seen
 
+@onready var wrong_tries_count: Label = $WrongTriesCount
+var wrong_count = 0
+
 # store ref to cards to hide them if matching
 var card1
 var card2
@@ -13,16 +16,18 @@ var texture_revealed # when card revealed
 var btn1_pressed = false
 var btn2_pressed = false
 
-var win_count 
-var how_many_good
+var win_count = 0
+var how_many_good = 0
+
+var children # to store rows of cards
 
 func _ready():
 	# load the normal texture
 	normal_texture = $VBoxContainer/Row1/TextureButton.texture_normal
-	win_count = 0
-	how_many_good = 0
+	#win_count = 0
+	#how_many_good = 0
 	
-	var children = v_box_container.get_children(false) # array of buttons (cards)
+	children = v_box_container.get_children(false) # array of rows
 	
 	for x in children: # rows
 		var cards = x.get_children(false) # cards in a certain row
@@ -91,3 +96,17 @@ func check_card2(c): # second card picked
 		card2.texture_focused = normal_texture
 		card2.texture_pressed = normal_texture
 		
+		wrong_count += 1
+		if wrong_count >= 10:
+			wrong_tries_count.text = ("YOU LOSE!!")
+		elif wrong_count >= 5:
+			wrong_tries_count.text = ("wrong tries count: " + str(wrong_count))
+			swap_rows()
+		else:
+			wrong_tries_count.text = ("wrong tries count: " + str(wrong_count))
+	
+		
+func swap_rows():
+	$VBoxContainer.move_child(children[1], 0)
+	#children[0].move_child($VBoxContainer/Row1/TextureButton3, 1) # row 1
+	
